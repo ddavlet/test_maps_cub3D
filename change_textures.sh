2027375e046bed631c7603bc03a12db51e699d1a
ddavlety <ddavlety@student.42berlin.de>
2024-05-11 17:52:23 +0200

    change textures script

--- change_textures.sh
+++ change_textures.sh
@@ -2,10 +2,19 @@
 
 for filename in ./*; do
     if [ "$filename" != "./replace" ] && [ "$filename" != "./replace.sh" ] && [ "$filename" != "./README.md" ]; then
-        ./replace "$filename" "textures/texture-01.png" "./src/textures/DarkAbstractBackgrounds_03.png"
-        ./replace "$filename" "textures/texture-02.png" "./src/textures/DarkAbstractBackgrounds_06.png"
-        ./replace "$filename" "textures/texture-03.png" "./src/textures/DarkAbstractBackgrounds_09.png"
-        ./replace "$filename" "textures/texture-04.png" "./src/textures/DarkAbstractBackgrounds_010.png"
+        ./replace "$filename" "./src/textures/DarkAbstractBackgrounds_03.png" "./src/textures/DarkAbstractBackgrounds_03.png"
+		rm "$filename"
+        cp "$filename.replace" "$filename"
+        rm "$filename.replace"
+        ./replace "$filename" "./src/textures/DarkAbstractBackgrounds_06.png" "./src/textures/DarkAbstractBackgrounds_06.png"
+		rm "$filename"
+        cp "$filename.replace" "$filename"
+        rm "$filename.replace"
+        ./replace "$filename" "./src/textures/DarkAbstractBackgrounds_09.png" "./src/textures/DarkAbstractBackgrounds_09.png"
+		rm "$filename"
+        cp "$filename.replace" "$filename"
+        rm "$filename.replace"
+        ./replace "$filename" "./src/textures/DarkAbstractBackgrounds_010.png" "./src/textures/DarkAbstractBackgrounds_010.png"
         rm "$filename"
         cp "$filename.replace" "$filename"
         rm "$filename.replace"
